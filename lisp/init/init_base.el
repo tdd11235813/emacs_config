0a089f12bd4dc01f47b7882c00ce7c7e7a1f1509
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-09-06 20:32:55 +0200

    Adds dump-jump package to base config.

--- lisp/init/init_base.el
+++ lisp/init/init_base.el
@@ -478,6 +478,16 @@ comma-separated columns."
 
 (setq company-dabbrev-downcase nil) ;; for case-sensitive autocompletion
 
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ;; ("M-g x" . dumb-jump-go-prefer-external)
+         ;; ("M-g z" . dumb-jump-go-prefer-external-other-window)
+         )
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+    :ensure)
+
 (defun save-all ()
   "Save all dirty buffers without asking for confirmation."
   (interactive)
