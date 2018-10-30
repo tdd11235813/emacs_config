69713f6bb3e550e2808672cae3f66ce157c8bc42
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-10-30 10:37:49 +0100

    Integrates init_scivis into init [WIP].
    - polymodes make troubles

--- init.el
+++ init.el
@@ -116,7 +116,179 @@
   ;; :config
   ;; (init-theme-dark 0)
   )
-(init-theme-dark 0)
+;;(init-theme-dark 0)
+
+(use-package gnuplot-mode
+  :ensure t
+  :config
+  (defvar gnuplot-program "/usr/bin/gnuplot")
+  (add-to-list 'auto-mode-alist '("\\.gnu$" . gnuplot-mode))
+  )
+
+;; after download just run make in lisp/ESS/ to byte-compile it
+(use-package ess-site
+  :load-path "lisp/ESS/lisp"
+  :ensure f
+  :config
+  (setq ess-history-file nil)
+  (ess-toggle-underscore nil)
+  )
+
+(use-package flyspell
+  :ensure t
+  :init
+  (setq flyspell-issue-message-flag nil)
+  (setq ispell-local-dictionary "english")
+  :config
+  (defun fd-switch-dictionary()
+    (interactive)
+    (let* ((dic ispell-current-dictionary)
+           (change (if (string= dic "deutsch8") "english" "deutsch8")))
+      (ispell-change-dictionary change)
+      (message "Dictionary switched from %s to %s" dic change)
+      ))
+
+  (global-set-key (kbd "<f9>")   'fd-switch-dictionary)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+  )
+
+(use-package tex-site
+  :ensure auctex
+  :init
+  ;; https://www.emacswiki.org/emacs/AUCTeX
+  (setq TeX-PDF-mode t)
+  ;;   (setq TeX-parse-self t) ;; parses packages
+  (setq-default TeX-master nil) ;; asks for master file after opening
+
+  (setq TeX-source-correlate-method 'synctex)
+  ;; ##### Enable synctex generation. Even though the command shows
+  ;; ##### as "latex" pdflatex is actually called
+  (custom-set-variables '(LaTeX-command "latex -synctex=1") )
+
+  ;; ##### Use Okular to open your document at the good
+  ;; ##### point. It can detect the master file.
+  (add-hook 'LaTeX-mode-hook '(lambda ()
+                                (add-to-list 'TeX-expand-list
+                                             '("%u" Okular-make-url))))
+
+  (defun Okular-make-url () (concat
+                             "file://"
+                             (expand-file-name (funcall file (TeX-output-extension) t)
+                                               (file-name-directory (TeX-master-file)))
+                             "#src:"
+                             (TeX-current-line)
+                             (expand-file-name (TeX-master-directory))
+                             "./"
+                             (TeX-current-file-name-master-relative)))
+
+  ;; ## Use these lines if you want a confirmation of the
+  ;; ## command line to run...
+  ;; (setq TeX-view-program-selection '((output-pdf "Okular")))
+  ;; (setq TeX-view-program-list '(("Okular" "okular --unique %u")))
+  ;; ## And theses if you don't want any confirmation.
+  (eval-after-load "tex"
+    '(add-to-list 'TeX-command-list
+                  '("View" "okular --unique %u" TeX-run-discard-or-function nil t :help "View file")))
+
+  (setq TeX-view-program-selection
+        '(((output-dvi has-no-display-manager) "dvi2tty")
+          ((output-dvi style-pstricks) "dvips and gv")
+          (output-pdf "Okular")
+          (output-dvi "xdvi")
+          (output-html "Palemoon")))
+  (setq TeX-view-program-list
+        '(("Palemoon" "palemoon %o")))
+  :config
+  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+  (add-hook 'LaTeX-mode-hook (lambda () (abbrev-mode +1)))
+  )
+
+(use-package reftex
+  :ensure t
+  :commands turn-on-reftex
+  :init
+  (setq reftex-plug-into-AUCTeX t)
+  )
+
+(use-package bibtex
+  :ensure t
+  :mode ("\\.bib" . bibtex-mode)
+  :init
+  (progn
+    (setq bibtex-align-at-equal-sign t)
+    (add-hook 'bibtex-mode-hook (lambda () (set-fill-column 120)))))
+
+;; enable if needed
+;; (use-package flycheck
+;;   :config
+;;   (add-hook 'prog-mode-hook 'flycheck-mode)
+;;   )
+
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown")
+  )
+
+;; polymode
+;; outdated: requires install.packages('rmarkdown') in R and pandoc
+;; updated: https://www.reddit.com/r/spacemacs/comments/9ciefe/polymode_for_rmd/
+;; requires specific packages
+;; FIXME: does not work at the moment
+(use-package polymode
+  :ensure t
+  :init
+  ;; (use-package poly-R
+  ;;   :ensure t)
+  ;; (use-package poly-noweb
+  ;;   :ensure t)
+  ;; (use-package poly-markdown
+  ;;   :ensure t)
+  ;;; MARKDOWN
+  (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+  ;;; R modes
+  (add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+;;;  (markdown-toggle-math t)
+
+  ;; ;; from https://gist.github.com/benmarwick/ee0f400b14af87a57e4a
+  ;; ;; compile rmarkdown to HTML or PDF with M-n s
+  ;; ;; use YAML in Rmd doc to specify the usual options
+  ;; ;; which can be seen at http://rmarkdown.rstudio.com/
+  ;; ;; thanks http://roughtheory.com/posts/ess-rmarkdown.html
+  ;; (defun ess-rmarkdown ()
+  ;;   "Compile R markdown (.Rmd). Should work for any output type."
+  ;;   (interactive)
+  ;;                                       ; Check if attached R-session
+  ;;   (condition-case nil
+  ;;       (ess-get-process)
+  ;;     (error
+  ;;      (ess-switch-process)))
+  ;;   (let* ((rmd-buf (current-buffer)))
+  ;;     (save-excursion
+  ;;       (let* ((sprocess (ess-get-process ess-current-process-name))
+  ;;              (sbuffer (process-buffer sprocess))
+  ;;              (buf-coding (symbol-name buffer-file-coding-system))
+  ;;              (R-cmd
+  ;;               (format "library(rmarkdown); rmarkdown::render(\"%s\")"
+  ;;                       buffer-file-name)))
+  ;;         (message "Running rmarkdown on %s" buffer-file-name)
+  ;;         (ess-execute R-cmd 'buffer nil nil)
+  ;;         (switch-to-buffer rmd-buf)
+  ;;         (ess-show-buffer (buffer-name sbuffer) nil)))))
+
+  ;; (define-key polymode-mode-map "\M-ns" 'ess-rmarkdown)
+  )
+
+(init-theme-dark 1)
+
 
 ;; for the custom variable definitions
 (setq custom-file "~/.emacs.d/custom.el")
